0668bdfdd00dd0c52327103c1ea011f5d7da9219
Fabian <fabianmariqueo@gmail.com>
2022-02-08 12:35:52 -0300

    feat: init project

---
+++ pubsub-demo/src/main/java/com/fmariqueo/pubsubdemo/PubsubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fmariqueo.pubsubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubDemoApplication.class, args);
+	}
+
+}
